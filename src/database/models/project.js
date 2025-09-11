5c860e28434b2854ff5b1892e89357ce77c22c76
Marc Munene <marcmunene104@gmail.com>
2025-09-11 21:24:29 +0300

    project model

---
+++ src/database/models/project.js
@@ -0,0 +1,24 @@
+import { Schema, model } from "mongoose";
+
+const projectSchema = new Schema(
+  {
+    client: { type: Schema.Types.ObjectId, ref: "client" },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    StartDate: { type: String, required: true },
+    deadline: { type: String, required: true },
+    budget: { type: Number, required: true },
+    status: {
+      type: string,
+      enum: ["Planned", "Ongoing", "completed"],
+      default: "Planned",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Project = new model("project", projectSchema);
+
+export { Project };
